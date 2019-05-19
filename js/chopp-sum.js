afbf62cadd2fe8056e9972ca9789351d43be3e1c
Lucas Leal <lealadiala@gmail.com>
2019-05-19 01:15:49 -0300

    first commit

---
+++ js/chopp-sum.js
@@ -0,0 +1,7 @@
+function choppcalc() {
+	var qntdman = document.getElementById(man);
+	var qntdwoman = document.getElementById(woman);
+	
+
+	window.alert(parseInt(qntdman) + parseInt(qntdwoman));
+}
